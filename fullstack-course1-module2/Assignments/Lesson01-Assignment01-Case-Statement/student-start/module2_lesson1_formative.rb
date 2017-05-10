1fba41f92409181ad880798be2c11e06878188f6
Tiffany Cheng <tchengj@gmail.com>
2017-05-10 19:49:43 +0800

    message

--- fullstack-course1-module2/Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ fullstack-course1-module2/Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,28 @@
 some_var = "false"
 another_var = "nil"
-
-if some_var == "pink elephant"
-  puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
-  puts "Question mark in the method name?"
-
-elsif some_var == false
-  puts "Looks like this one should execute"
-
-else
-  puts "I guess nothing matched... But why?"
+pink_elephant = "pink elephant"
+case 
+  when pink_elephant == "pink elephant"
+  	puts "Don't think about the pink elephant!"
+  when another_var == "nil"
+  	puts "Question mark in the method name?"
+  when some_var == "false"
+  	puts "Looks like this one should execute"
+  else
+  	puts "I guess nothing matched... But why?"
 end
+	
+
+#original statement:-
+#if some_var == "pink elephant"
+#  puts "Don't think about the pink elephant!"
+
+#elsif another_var.nil?
+#  puts "Question mark in the method name?"
+
+#elsif some_var == false
+#  puts "Looks like this one should execute"
+
+#else
+#  puts "I guess nothing matched... But why?"
+#end
